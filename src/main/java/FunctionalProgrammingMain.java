2290452a1d061cd4b644fb9fe2521b34b8d5c3be
Yuvaraj <yuvaraj.sf.java@gmail.com>
2025-09-19 17:22:46 +0530

    Map Stream operations

--- src/main/java/FunctionalProgrammingMain.java
+++ src/main/java/FunctionalProgrammingMain.java
@@ -4,6 +4,7 @@ import com.functional.streams.EmployeeStreamProcessor;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 public class FunctionalProgrammingMain {
   public static void main(String[] args) {
@@ -25,7 +26,28 @@ public class FunctionalProgrammingMain {
     ·Converts their names to UPPERCASE
     ·Sorts the list by name in descending order
     ·Returns the result as a List<Emp>*/
-    List<Employee> employees = EmployeeStreamProcessor.processEmployees(Employee.getDummyEmployees());
-    System.out.println(employees);
+    /*List<Employee> employees = EmployeeStreamProcessor.processEmployees(Employee.getDummyEmployees());
+    System.out.println(employees);*/
+
+    /*convert List<Employee> into Map<empName,salary>*/
+    Map<String, Double> map = EmployeeStreamProcessor.convertToMap(Employee.getDummyEmployees());
+    System.out.println(map);
+
+    //sort map by Key
+    Map<String, Double> sortByKey = EmployeeStreamProcessor.sortMapByKey(map);
+    System.out.println(sortByKey);
+
+    //sort map by Key Desc
+    Map<String, Double> sortByKeyDesc = EmployeeStreamProcessor.sortMapByKeyDesc(map);
+    System.out.println(sortByKeyDesc);
+
+    //sort map by Value
+    Map<String, Double> sortByValue = EmployeeStreamProcessor.sortMapByValue(map);
+    System.out.println(sortByValue);
+
+    //sort map by Value Desc
+    Map<String, Double> sortByValueDesc = EmployeeStreamProcessor.sortMapByValueDesc(map);
+    System.out.println(sortByValueDesc);
+
   }
 }
--- src/main/java/com/functional/streams/EmployeeStreamProcessor.java
+++ src/main/java/com/functional/streams/EmployeeStreamProcessor.java
@@ -3,7 +3,9 @@ package com.functional.streams;
 import com.functional.model.Employee;
 
 import java.util.Comparator;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 /*Given a List<Emp>:
@@ -13,18 +15,68 @@ import java.util.stream.Collectors;
 ·Returns the result as a List<Emp>*/
 
 public class EmployeeStreamProcessor {
+
   public static List<Employee> processEmployees(List<Employee> list){
     System.out.println("Given list of employees:");
     list.stream()
             .forEach(emp-> System.out.println(emp.getName()+"("+emp.getSalary()+")"));
     System.out.println("*".repeat(20));
+
     List<Employee> collect = list.stream()
             .filter(emp -> emp.getSalary() > 50000)
             .map(emp ->  new Employee(emp.getName().toUpperCase(),emp.getSalary()))
             .sorted(Comparator.comparing(Employee :: getName).reversed())
             .collect(Collectors.toList());
     return collect;
+  }
 
+  public static Map<String,Double> convertToMap(List<Employee> list){
+    return list.stream()
+            .collect(Collectors.toMap(Employee::getName,Employee::getSalary));
+  }
+
+  public static Map<String,Double> sortMapByKey(Map<String,Double> map){
+    return map.entrySet().stream()
+            .sorted(Map.Entry.comparingByKey())
+            .collect(
+                    Collectors.toMap(
+                            Map.Entry::getKey,
+                            Map.Entry::getValue,
+                            (e1, e2) -> e1,
+                            LinkedHashMap::new));
+  }
+
+  public static Map<String,Double> sortMapByKeyDesc(Map<String,Double> map){
+    return map.entrySet().stream()
+            .sorted(Map.Entry.<String,Double>comparingByKey().reversed())
+            .collect(
+                    Collectors.toMap(
+                            Map.Entry::getKey,
+                            Map.Entry::getValue,
+                            (e1, e2) -> e1,
+                            LinkedHashMap::new));
+  }
+
+  public static Map<String,Double> sortMapByValue(Map<String,Double> map){
+    return map.entrySet().stream()
+            .sorted(Map.Entry.comparingByValue())
+            .collect(
+                    Collectors.toMap(
+                            Map.Entry::getKey,
+                            Map.Entry::getValue,
+                            (e1, e2) -> e1,
+                            LinkedHashMap::new));
+  }
+
+  public static Map<String,Double> sortMapByValueDesc(Map<String,Double> map){
+    return map.entrySet().stream()
+            .sorted(Map.Entry.<String,Double>comparingByValue().reversed())
+            .collect(
+                    Collectors.toMap(
+                            Map.Entry::getKey,
+                            Map.Entry::getValue,
+                            (e1, e2) -> e1,
+                            LinkedHashMap::new));
   }
 }
 
